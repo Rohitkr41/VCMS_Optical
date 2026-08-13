ef9b170474dcdd3d4acb577642043ff1e9fb7510
Rohit Kumar <138009969+Rohitkr41@users.noreply.github.com>
2026-08-13 18:05:58 +0530

    Update ClinicalTest.java

--- src/test/java/tests/ClinicalTest.java
+++ src/test/java/tests/ClinicalTest.java
@@ -32,7 +32,7 @@ public class ClinicalTest extends BaseTest {
         ClinicalPage clinical = new ClinicalPage(driver);
 
         // Perform search with date filter
-        clinical.searchByDate("01/07/2026", "31/09/2026");
+        clinical.searchByDate("01/08/2026", "31/09/2026");
 
         // Click icon for "New" status row
         clinical.clickNewStatusIcon();
