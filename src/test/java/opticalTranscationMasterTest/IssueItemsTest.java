50214b3ee145a928718ba61162b4c96f1157d4af
Rohit Kumar <138009969+Rohitkr41@users.noreply.github.com>
2026-08-14 09:14:01 +0530

    Update IssueItemsTest.java

--- src/test/java/opticalTranscationMasterTest/IssueItemsTest.java
+++ src/test/java/opticalTranscationMasterTest/IssueItemsTest.java
@@ -46,8 +46,8 @@ public class IssueItemsTest extends BaseTest {
         issuePage.clickSearchButton();
 
         // STEP-3 ISSUE QTY
-        issuePage.enterIssueQty("CA26-0004", "5");
-        issuePage.enterIssueQty("CA26-0005", "6");
+        issuePage.enterIssueQty("CA26-0004", "4");
+        issuePage.enterIssueQty("CA26-0005", "3");
         issuePage.enterIssueQty("CL26-0004", "4");
 
         // STEP-4 SELECT VC
