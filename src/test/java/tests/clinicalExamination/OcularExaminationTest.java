29c9bd473d90cba82e3ff7e79ef8a72aba809afe
Rohit Kumar <138009969+Rohitkr41@users.noreply.github.com>
2026-09-16 09:16:23 +0530

    Update OcularExaminationTest.java

--- src/test/java/tests/clinicalExamination/OcularExaminationTest.java
+++ src/test/java/tests/clinicalExamination/OcularExaminationTest.java
@@ -36,7 +36,7 @@ public class OcularExaminationTest extends BaseTest {
 
             // ===== OPEN PATIENT =====
             ClinicalPage clinical = new ClinicalPage(driver);
-            clinical.searchByDate("01/08/2026", "30/09/2026");
+            clinical.searchByDate("01/09/2026", "30/10/2026");
             clinical.clickNewStatusIcon();
             System.out.println("✅ Patient opened");
 
